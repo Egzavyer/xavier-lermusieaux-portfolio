0009d987921db23aa4269194ec9306c95a56bf41
Egzavyer <xavier.lermusieaux@gmail.com>
2023-12-12 17:05:26 -0500

    Fix: fixes navbar responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-key */
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { Bars4Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
@@ -12,27 +12,52 @@ const Header = () => {
 	];
 
 	let [isOpen, setIsOpen] = useState(false);
+	let [isTransition, setIsTransition] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+		setIsTransition(true);
+	};
+
+	// Function to handle resize event
+	const handleResize = () => {
+		if (window.innerWidth > 1024) {
+			setIsOpen(false);
+			setIsTransition(false);
+		}
+	};
+
+	// Add event listener on component mount and remove on unmount
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	return (
 		<div className='shadow-md w-full fixed top-0 left-0 bg-headerFooterBgColor text-headerFooterTextColor whitespace-nowrap'>
-			<div className='flex lg:px-10 py-4 px-7 lg:flex justify-between'>
-				{/*Logo*/}
-				<div className='flex text-2xl cursor-pointer gap-1 '>
-					<span className='font-bold'>Xavier Lermusieaux</span>
-				</div>
-
-				{/*Mobile Nav*/}
-				<div
-					onClick={() => setIsOpen(!isOpen)}
-					className='w-7 h-7 absolute right-8 top-6 cursor-pointer lg:hidden'
-				>
-					{isOpen ? <XMarkIcon /> : <Bars4Icon />}
+			<div className='flex lg:px-10 py-4 px-7 lg:flex items-center justify-between'>
+				{/*Logo and Mobile Nav*/}
+				<div className='flex items-center'>
+					<div className='flex text-2xl cursor-pointer gap-1'>
+						<span className='font-bold'>Xavier Lermusieaux</span>
+					</div>
+					<div
+						onClick={toggleDropdown}
+						className='w-7 h-7 absolute right-8 top-6 cursor-pointer lg:hidden'
+					>
+						{isOpen ? <XMarkIcon /> : <Bars4Icon />}
+					</div>
 				</div>
 
 				{/*NavLinks*/}
 				<ul
-					className={`lg:flex pl-9 lg:pl-0 lg:items-center lg:pb-0 pb-12 absolute lg:z-auto z-[-1] left-0 w-full transition-all bg-headerFooterBgColor duration-500 ease-in ${
-						isOpen ? "top-12" : "hidden"
+					className={`lg:flex pl-9 lg:pl-0 lg:items-center lg:pb-0 pb-12 absolute lg:relative lg:z-auto z-[-1] left-0 w-full justify-end ${
+						isTransition ? "transition-all duration-500 ease-in" : ""
+					} bg-headerFooterBgColor ${
+						isOpen ? "top-12 lg:top-auto" : "top-[-490px] lg:top-auto"
 					}`}
 				>
 					{Links.map((link) => (
